40781a2070fd6aac39bee107003bef6125e78a54
obleus <odney@Odneys-MacBook-Air.local>
2022-03-06 07:45:27 -0500

    added questions

--- index.js
+++ index.js
@@ -1,9 +1,61 @@
-const fs = require('fs')
-const generatePage = require('./src/page-template')
+const fs = require('fs');
+const util = requires ('util');
+const inquirer = require('inquirer');
 
-
-
-
-
-
+const writeFileAsync = util.promisify(fs.writeFile);
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "author",
+        message: "What is the author's name?"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a brief description of your project:"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: ["MIT","APACHE 2.0","GPL 3.0","BSD 3","None"]
+    },
+    {
+        type: "input",
+        name: "installations",
+        message: "What command should be run to install dependencies?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "What command should be run to run tests?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What does the user need to know about using the repo?"
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message: "What does the user need to know about contributing to the repo?",
+    },
+]);
+  };
 
