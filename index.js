e1b1d40c8a004a332c3059e5e250369807058e9a
obleus <odney@Odneys-MacBook-Air.local>
2022-03-06 17:54:42 -0500

    updated init function'

--- index.js
+++ index.js
@@ -140,5 +140,16 @@ generateMD(data)))
   .then(() => console.log('Sucess'))
   .catch((err) => console.error(err));
 
+// Function to initialize the app
+function init() {
+  return inquirer.prompt(questions);
+}
+
+// Function call to initialize app
+init()
+  .then((projectData) => {
+    return generateMarkdown(projectData);
+  })
+  .then((markdown) => writeToFile('README.md', markdown));
 
   
